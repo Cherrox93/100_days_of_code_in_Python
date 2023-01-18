60c65f59a4a6eed7b61c7b465f5205c06b39a1f4
Kamil Kapłon <122369882+Cherrox93@users.noreply.github.com>
2023-01-18 12:03:24 +0100

    Create 1.1.py

---
+++ Day 01/Exercises/1.1.py
@@ -0,0 +1,15 @@
+#Instructions
+#Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function.
+
+#Warning: The output in your program should match the example output shown below exactly, character for character, even spaces and symbols should be identical, otherwise the tests won't pass.
+
+#Example Output
+#After you have written your code, you should run your program and it should print the following:
+
+![1 1  print](https://user-images.githubusercontent.com/122369882/213155219-f3cecd99-e6b4-44e6-8b15-7d514ad8c9bb.gif)
+
+#Write your code below this line 👇
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
